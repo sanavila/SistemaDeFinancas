43c107b1ea2b104fd5247b41168a0b777ae11758
San Ávila <rute.leone@hotmail.com>
2022-11-21 08:49:40 -0300

    Estruturação da página principal com return de HTML e estilização com StyleComponents. Criação de arrays de items

---
+++ src/data/items.ts
@@ -0,0 +1,6 @@
+export const items = [
+    { data: new Date( 2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12},
+    { data: new Date( 2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12},
+    { data: new Date( 2021, 9, 16), category: 'rent', title: 'Aluguel Apt', value: 2300},
+    { data: new Date( 2021, 10, 18), category: 'salary', title: 'Salário ACME', value: 4500}
+];
