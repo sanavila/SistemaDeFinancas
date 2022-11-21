d2ca50539fbc748a0d9d31bba0d34c027709a577
San Ávila <rute.leone@hotmail.com>
2022-11-21 08:59:43 -0300

    Tipando array de item

--- src/data/items.ts
+++ src/data/items.ts
@@ -1,6 +1,8 @@
-export const items = [
-    { data: new Date( 2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12},
-    { data: new Date( 2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12},
-    { data: new Date( 2021, 9, 16), category: 'rent', title: 'Aluguel Apt', value: 2300},
-    { data: new Date( 2021, 10, 18), category: 'salary', title: 'Salário ACME', value: 4500}
+import { Item } from '../types/item';
+
+export const items: Item[] = [
+    { date: new Date( 2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12},
+    { date: new Date( 2021, 9, 15), category: 'food', title: 'McDonalds', value: 32.12},
+    { date: new Date( 2021, 9, 16), category: 'rent', title: 'Aluguel Apt', value: 2300},
+    { date: new Date( 2021, 10, 18), category: 'salary', title: 'Salário ACME', value: 4500}
 ];
